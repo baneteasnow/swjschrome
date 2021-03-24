a06b6d7df2bff623e70fcdf5c2ad274586b80f04
baneteasnow <81329385+baneteasnow@users.noreply.github.com>
2021-03-25 10:17:10 +1100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.open('pwa-example').then(function(cache) {
+            return cache.addAll([
+                'https://www.thebookish.de/',
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+});
